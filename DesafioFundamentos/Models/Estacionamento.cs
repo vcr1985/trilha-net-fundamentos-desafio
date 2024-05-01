1517240268b1f659029488c642a448d871e15161
Vando Ramos <ramosvando@gmail.com>
2024-05-01 18:20:53 -0300

    Desafio concluido

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
@@ -14,32 +18,36 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+#pragma warning disable CS8600 // Conversão de literal nula ou possível valor nulo em tipo não anulável.
+            string placa = Console.ReadLine();
+#pragma warning restore CS8600 // Conversão de literal nula ou possível valor nulo em tipo não anulável.
+
+#pragma warning disable CS8604 // Possível argumento de referência nula.
+            veiculos.Add(placa);
+#pragma warning restore CS8604 // Possível argumento de referência nula.
         }
 
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
+#pragma warning disable CS8600 // Conversão de literal nula ou possível valor nulo em tipo não anulável.
+            string placa = Console.ReadLine();
+#pragma warning restore CS8600 // Conversão de literal nula ou possível valor nulo em tipo não anulável.
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
-
-            // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            if (veiculos.Any(x => x.Equals(placa, StringComparison.CurrentCultureIgnoreCase)))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                int horas;
+                while (!int.TryParse(Console.ReadLine(), out horas) || horas < 0)
+                {
+                    Console.WriteLine("Valor inválido. Digite a quantidade de horas novamente:");
+                }
+                
+                decimal valorTotal = precoInicial + (precoPorHora * horas);
+#pragma warning disable CS8604 // Possível argumento de referência nula.
+                _ = veiculos.Remove(placa);
+#pragma warning restore CS8604 // Possível argumento de referência nula.
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
@@ -50,18 +58,22 @@ namespace DesafioFundamentos.Models
         }
 
         public void ListarVeiculos()
+{
+    // Verifica se há veículos no estacionamento
+    if (veiculos.Any())
+    {
+        Console.WriteLine("Os veículos estacionados são:");
+        foreach (var veiculo in veiculos)
         {
-            // Verifica se há veículos no estacionamento
-            if (veiculos.Any())
-            {
-                Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
-            }
-            else
-            {
-                Console.WriteLine("Não há veículos estacionados.");
-            }
+            Console.WriteLine(veiculo);
         }
     }
+    else
+    {
+        Console.WriteLine("Não há veículos estacionados.");
+    }
 }
+
+}
+}
+
